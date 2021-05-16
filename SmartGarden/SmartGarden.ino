11a4235026b6c0ff04465038925d653998c078bc
SimonWWWW <winklarz.szymon@gmail.com>
2021-05-16 17:29:23 +0200

    Dodanie komentarzy

--- SmartGarden/SmartGarden.ino
+++ SmartGarden/SmartGarden.ino
@@ -10,7 +10,7 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("Wilgotnosc gleby wynosi: ");
-  Serial.println(sprawdzanieWilgotnosciGleby());
+  Serial.print("Wilgotnosc gleby wynosi: "); //testowe wyświetlanie 
+  Serial.println(sprawdzanieWilgotnosciGleby());//testowe wyświetlanie 
 
 }
