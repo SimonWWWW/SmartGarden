cb9e6c5de328061064c255965251ad02bb669d00
SimonWWWW <winklarz.szymon@gmail.com>
2021-06-20 00:33:50 +0200

    Poprawienie nazw na angielskie + czujnik DHT od Konrada

--- SmartGarden/SmartGarden.ino
+++ SmartGarden/SmartGarden.ino
@@ -1,23 +1,35 @@
-#define czujnikWilgotnosciGleby A0 //przypisanie pinu A0 do czujnika wilgotnosci gleby
+#include "DHT.h"
+#define groundHumiditySensor A0 //przypisanie pinu A0 do czujnika wilgotnosci gleby
 //Jako sensor wykorzystana jest miniaturowa turbina wiatrowa. Napięcie generowane przez turbinę jest monitorowane na porcie szeregowym.
 //Kiedy wartość przekracza próg markiza jest zamykana (lub nie otwiera się). Co do wartości potrzeba wygrzebać przelicznik lub kalibrację.
 #define WIND A1 //przypisanie pinu A1 do czujnika prędkości wiatru
 #define LightSensor A2 //przypisanie pinu A2 do czujnika nasłonecznienia
+#define DHTTYPE DHT11 // PIN 3 - czujnik DHT11
+#define DHTPIN 2  
 
 int windValue1; //definicja wartości pomiarów wiatru
 int windValue2;
 int windValue3;
+DHT dht(DHTPIN, DHTTYPE);
 
-int wilgotnoscGleby = 0;
-int sprawdzanieWilgotnosciGleby(){
-  wilgotnoscGleby = map(analogRead(czujnikWilgotnosciGleby),0,620,0,100); //mapowanie wartości analogowej na wartosc  procentowa (wartosci 0 do 620 uzyskane doswiadczalnie)
-  return wilgotnoscGleby;
+float temperature;
+float airHumidity;
+
+int groundHumidity;
+int groundHumidityFunction(){
+  groundHumidity = map(analogRead(groundHumiditySensor),0,620,0,100); //mapowanie wartości analogowej na wartosc  procentowa (wartosci 0 do 620 uzyskane doswiadczalnie)
+  return groundHumidity;
 }
 void setup() {
   Serial.begin(9600);
+  dht.begin();
   
-  //startowy reset wartosci wiatru
+  //startowy reset wartosci 
+  temperature = 0;
+  airHumidity = 0;
+  groundHumidity = 0;
   windValue3 = 0;
+  groundHumidity = 0;
 }
 
 void loop() {
@@ -26,12 +38,20 @@ void loop() {
   Serial.println(lightSensorValue, DEC);
   
   Serial.print("Wilgotnosc gleby wynosi: "); //testowe wyświetlanie 
-  Serial.println(sprawdzanieWilgotnosciGleby());//testowe wyświetlanie 
+  Serial.println(groundHumidityFunction());//testowe wyświetlanie 
   
   //siła wiatru************start
   //podanie aktualej na serial
   Serial.print("Siła wiatru: ");
   Serial.println(WIND);
+  
+  temperature = dht.readTemperature();
+  airHumidity = dht.readHumidity();
+  
+  Serial.print("Temperatura: ");
+  Serial.println(temperature);
+  Serial.print("Wilgotnosc powietrza: ");
+  Serial.println(airHumidity);
 
   //przeniesienie wartości do buforów
   windValue1 = windValue2;
@@ -47,7 +67,7 @@ void loop() {
         //zraszacze wyłączone
        }
 
-   delay(6000); //opóźnienie między pomiarami
+   delay(4000); //opóźnienie między pomiarami
   //siła wiatru***************end
 
 }
