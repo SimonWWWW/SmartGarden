32947abe8543a488575cf3bb738ec772e60d0b79
VeeTalick <78079173+VeeTalick@users.noreply.github.com>
2021-06-24 22:41:48 +0200

    Add files via upload

--- SmartGarden/SmartGarden.ino
+++ SmartGarden/SmartGarden.ino
@@ -1,4 +1,5 @@
-#include "DHT.h"
+#include <buildTime.h>
+#include "TinyDHT.h"
 #define groundHumiditySensor A0 //przypisanie pinu A0 do czujnika wilgotnosci gleby
 //Jako sensor wykorzystana jest miniaturowa turbina wiatrowa. Napięcie generowane przez turbinę jest monitorowane na porcie szeregowym.
 //Kiedy wartość przekracza próg markiza jest zamykana (lub nie otwiera się). Co do wartości potrzeba wygrzebać przelicznik lub kalibrację.
@@ -20,6 +21,12 @@ int groundHumidityFunction(){
   groundHumidity = map(analogRead(groundHumiditySensor),0,620,0,100); //mapowanie wartości analogowej na wartosc  procentowa (wartosci 0 do 620 uzyskane doswiadczalnie)
   return groundHumidity;
 }
+int lightSensor;
+int lightSensorValue(){
+  lightSensor = map(analogRead(LightSensor), 0, 1023, 100, 0); //mapowanie wartości analogowej na wartość  procentową
+  return lightSensor;
+}
+
 void setup() {
   Serial.begin(9600);
   dht.begin();
@@ -29,29 +36,12 @@ void setup() {
   airHumidity = 0;
   groundHumidity = 0;
   windValue3 = 0;
-  groundHumidity = 0;
 }
 
 void loop() {
-  int lightSensorValue = analogRead(LightSensor);
-  Serial.print("Nasłonecznienie: "); //testowe wyświetlanie
-  Serial.println(lightSensorValue, DEC);
-  
-  Serial.print("Wilgotnosc gleby wynosi: "); //testowe wyświetlanie 
-  Serial.println(groundHumidityFunction());//testowe wyświetlanie 
-  
-  //siła wiatru************start
-  //podanie aktualej na serial
-  Serial.print("Siła wiatru: ");
-  Serial.println(WIND);
-  
+
   temperature = dht.readTemperature();
   airHumidity = dht.readHumidity();
-  
-  Serial.print("Temperatura: ");
-  Serial.println(temperature);
-  Serial.print("Wilgotnosc powietrza: ");
-  Serial.println(airHumidity);
 
   //przeniesienie wartości do buforów
   windValue1 = windValue2;
@@ -59,15 +49,79 @@ void loop() {
 
   //przypisanie aktulanej wartości
   windValue3 = WIND;
+  
+  Serial.print("Godzina: ");
+  Serial.println(BUILD_HOUR);
 
-  //test 3 ostatnich wartości
-  if(windValue1 > 350 && windValue2 > 350 && windValue3 > 350)
-       {
-        //markiza zamknięta
-        //zraszacze wyłączone
+  Serial.print("Miesiąc: ");
+  Serial.println(BUILD_MONTH);
+
+  Serial.print("Nasłonecznienie: "); //testowe wyświetlanie
+  Serial.println(lightSensorValue());
+ 
+  Serial.print("Wilgotnosc gleby wynosi: "); //testowe wyświetlanie
+  Serial.println(groundHumidityFunction());//testowe wyświetlanie
+ 
+  Serial.print("Siła wiatru: ");
+  Serial.println(WIND);
+
+  Serial.print("Temperatura: ");
+  Serial.println(temperature);
+  
+  Serial.print("Wilgotnosc powietrza: ");
+  Serial.println(airHumidity);
+
+  //logika dla markizy
+  if(BUILD_HOUR > 6 && BUILD_HOUR < 20){
+    if(windValue1 < 350 && windValue2 < 350 && windValue3 < 350){//test 3 ostatnich wartości
+        //tu wpisac otwieranie markizy
+        Serial.println("Markiza otwarta");//markiza otwarta
+       }
+    else{
+        //tu wpisac zamykanie markizy
+        Serial.println("Wieje silny wiatr, markiza zamknięta");//markiza zamknięta
        }
 
-   delay(4000); //opóźnienie między pomiarami
-  //siła wiatru***************end
+  }
+  else{
+    Serial.println("O tej godzinie markiza zamknięta");//markiza zamknięta
+  }
+
+  //logika dla podlewu
+  if(BUILD_MONTH > 3 && BUILD_MONTH < 10){//data od 1 kwietnia do 30 września
+    if(BUILD_HOUR > 18 && BUILD_HOUR < 24 || BUILD_HOUR >= 0 && BUILD_HOUR < 9){//czas od 19:00 do 8:59
+      if(windValue1 < 350 && windValue2 < 350 && windValue3 < 350){//test 3 ostatnich wartości
+        if(groundHumidityFunction() < 30){//zmienić wartość wilgotności głeby
+          if(lightSensorValue() < 20){//nasłonecznienie do 19%
+            if(temperature > 2){//temperatura powyżej 2 stopni
+              //tu wpisać włączenie podlewania
+              Serial.println("Podlew włączony");//Podlew włączony
+            }
+            else{
+              Serial.println("Jest zimno, podlew wyłączony");//Podlew wyłączony
+            }
+          }
+          else{
+            Serial.println("Słońce jasno świeci, podlew wyłączony");//Podlew wyłączony
+          }
+        }
+        else{
+          Serial.println("Wilgotność dostateczna, podlew wyłączony");//Podlew wyłączony
+        }
+      }
+      else{
+        Serial.println("Wieje silny wiatr, podlew wyłączony");//Podlew wyłączony
+      }
+    }
+    else{
+      Serial.println("O tej godzinie podlew wyłączony");//Podlew wyłączony
+    }
+  }
+  else{
+    Serial.println("W tym miesiącu podlew wyłączony");//Podlew wyłączony
+  }
+
+  Serial.println("");//odstęp
+  delay(4000); //opóźnienie między pomiarami
 
 }
