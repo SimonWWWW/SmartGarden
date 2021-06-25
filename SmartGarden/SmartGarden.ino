d7645156fb561bfad541aa55a08d6aed8eaaaab4
SimonWWWW <winklarz.szymon@gmail.com>
2021-06-25 19:23:03 +0200

    Add files via upload

--- SmartGarden/SmartGarden.ino
+++ SmartGarden/SmartGarden.ino
@@ -4,22 +4,27 @@
 //Kiedy wartość przekracza próg markiza jest zamykana (lub nie otwiera się). Co do wartości potrzeba wygrzebać przelicznik lub kalibrację.
 #define WIND A1 //przypisanie pinu A1 do czujnika prędkości wiatru
 #define LightSensor A2 //przypisanie pinu A2 do czujnika nasłonecznienia
-#define DHTTYPE DHT11 // PIN 3 - czujnik DHT11
-#define DHTPIN 2  
+#define DHTTYPE DHT11 //TYP DHT11
+#define DHTPIN 3  // PIN 3 - czujnik DHT11
+
 
 int windValue1; //definicja wartości pomiarów wiatru
 int windValue2;
 int windValue3;
 DHT dht(DHTPIN, DHTTYPE);
 
-float temperature;
-float airHumidity;
+int lightSensorValue;
+  
+int temperature;
+int airHumidity;
+
+int pressure;
 
 int groundHumidity;
-int groundHumidityFunction(){
-  groundHumidity = map(analogRead(groundHumiditySensor),0,620,0,100); //mapowanie wartości analogowej na wartosc  procentowa (wartosci 0 do 620 uzyskane doswiadczalnie)
-  return groundHumidity;
-}
+
+String Podlewanie;
+String Markiza;
+
 void setup() {
   Serial.begin(9600);
   dht.begin();
@@ -30,31 +35,51 @@ void setup() {
   groundHumidity = 0;
   windValue3 = 0;
   groundHumidity = 0;
+  pressure = 0;
+  lightSensorValue = 0;
+  
+
 }
 
-void loop() {
-  int lightSensorValue = analogRead(LightSensor);
-  Serial.print("Nasłonecznienie: "); //testowe wyświetlanie
-  Serial.println(lightSensorValue, DEC);
+void loop() {  
+  lightSensorValue = analogRead(LightSensor);
+  //Serial.println(lightSensorValue);
+
   
-  Serial.print("Wilgotnosc gleby wynosi: "); //testowe wyświetlanie 
-  Serial.println(groundHumidityFunction());//testowe wyświetlanie 
+  groundHumidity = map(analogRead(groundHumiditySensor),0,620,0,100); //mapowanie wartości analogowej na wartosc  procentowa (wartosci 0 do 620 uzyskane doswiadczalnie)
+  //Serial.println(groundHumidity);//testowe wyświetlanie 
   
-  //siła wiatru************start
-  //podanie aktualej na serial
-  Serial.print("Siła wiatru: ");
-  Serial.println(WIND);
   
   temperature = dht.readTemperature();
   airHumidity = dht.readHumidity();
-  
-  Serial.print("Temperatura: ");
-  Serial.println(temperature);
-  Serial.print("Wilgotnosc powietrza: ");
-  Serial.println(airHumidity);
 
+  /*Serial.println(String(temperature));
+  Serial.println(String(0000));
+  Serial.println(String(airHumidity));
+  Serial.println(String(WIND));
+  Serial.println(String(lightSensorValue));
+  Serial.println(String(groundHumidity));*/
+  //siła wiatru***************end
+  delay(5000); //opóźnienie między pomiarami
+  Serial.println("__________________________");
+  Serial.print("Temperatura: "); 
+  Serial.println((String)temperature);
+  Serial.print("Cisnienie: ");
+  Serial.println((String)pressure);
+  Serial.print("Wilgotnosc powietrza: ");
+  Serial.println((String)airHumidity);
+  Serial.print("Sila wiatru: ");
+  Serial.println((String)windValue3); 
+  Serial.print("Naslonecznienie: ");
+  Serial.println((String)lightSensorValue);
+  Serial.print("Wilgotnosc gleby: ");
+  Serial.println((String)groundHumidity);
+  Serial.print("Stan markizy: ");
+  Serial.println(Markiza);
+  Serial.print("Stan podlewania: ");
+  Serial.println(Podlewanie);
   //przeniesienie wartości do buforów
-  windValue1 = windValue2;
+ /* windValue1 = windValue2;
   windValue2 = windValue3;
 
   //przypisanie aktulanej wartości
@@ -66,8 +91,7 @@ void loop() {
         //markiza zamknięta
         //zraszacze wyłączone
        }
-
-   delay(4000); //opóźnienie między pomiarami
+  */
   //siła wiatru***************end
 
 }
